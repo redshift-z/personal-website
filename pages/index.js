a0594199e3ca5e2243770209aba63adefbf80321
razitaa <89266858+razitaa@users.noreply.github.com>
2024-01-23 02:21:56 +0700

    add intro styling

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,9 @@ export default function Home() {
       <Head>
         <title>hey!</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet"></link>
       </Head>
 
       <main className='grid grid-cols-2'>
