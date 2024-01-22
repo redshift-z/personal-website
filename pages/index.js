387aa1cc13245919d229f3c24cd4c84d6a9a5317
razitaa <89266858+razitaa@users.noreply.github.com>
2024-01-23 01:33:23 +0700

    add intro

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,21 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>hey!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+      <main className='grid-container'>
+        <Header className='grid-item' title="hey!" />
+        <p className="grid-item description">
+          I'm Razita Nuriyan, but please do call me Zita!! Currently, I'm a final year computer science student at the 
+          University of Indonesia, having just recently came back from a semester at the Newcastle University under the IISMA
+          (Indonesian International Student Mobility Awards) 2023 scheme. During my study, I've had the chance to explore a broad
+          range of subjects related to computing, but my experiences focused on Web Development, Computational Statistics, and High
+          Performance Computing. I'm currently doing research on a high-performance molecular dynamics simulator, as I've always 
+          been passionate about science for as long as I can remember (I've represented Indonesia in an international astrophysics
+          competition in high school and went on to teach astrophysics for 3 years, helping 20++ students win medals at national
+          and international levels). I can probably still name the stars at the night sky if we go stargazing 🔭.
         </p>
       </main>
 
